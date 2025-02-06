b099774dd38a3a79845e295d1bea7320688a13d2
Jean Massucatto <massucattoj@Jeans-MacBook-Pro.local>
2025-02-06 10:24:55 -0300

    button - button variants

---
+++ src/components/upload-widget-header.tsx
@@ -0,0 +1,14 @@
+import { Minimize2 } from "lucide-react";
+import { Button } from "./ui/button";
+
+export function UploadWidgetHeader() {
+  return (
+    <div className="w-full p-4 py-2 bg-white/2 border-zinc-800 border-b flex items-center justify-between">
+      <span className="text-sm font-medium">Upload files</span>
+
+      <Button size="icon" className="-mr-2">
+        <Minimize2 strokeWidth={1.5} className="size-4" />
+      </Button>
+    </div>
+  )
+}
