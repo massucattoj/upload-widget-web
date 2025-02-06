1576b874b066188e508ac8edef441a00e114ed88
Jean Massucatto <massucattoj@Jeans-MacBook-Pro.local>
2025-02-06 10:53:21 -0300

    feat: collapsible component

--- src/components/upload-widget-header.tsx
+++ src/components/upload-widget-header.tsx
@@ -1,3 +1,5 @@
+import * as Collapsible from "@radix-ui/react-collapsible";
+
 import { Minimize2 } from "lucide-react";
 import { Button } from "./ui/button";
 
@@ -6,9 +8,11 @@ export function UploadWidgetHeader() {
     <div className="w-full p-4 py-2 bg-white/2 border-zinc-800 border-b flex items-center justify-between">
       <span className="text-sm font-medium">Upload files</span>
 
-      <Button size="icon" className="-mr-2">
-        <Minimize2 strokeWidth={1.5} className="size-4" />
-      </Button>
+      <Collapsible.Trigger asChild> 
+        <Button size="icon" className="-mr-2">
+          <Minimize2 strokeWidth={1.5} className="size-4" />
+        </Button>
+      </Collapsible.Trigger>
     </div>
   )
 }
--- src/components/upload-widget.tsx
+++ src/components/upload-widget.tsx
@@ -1,19 +1,32 @@
+import * as Collapsible from "@radix-ui/react-collapsible";
 import { UploadWidgetDropzone } from "./upload-widget-dropzone";
 import { UploadWidgetHeader } from "./upload-widget-header";
 import { UploadWidgetUploadList } from "./upload-widget-upload-list";
+import React from "react";
+import { UploadWidgetMinimizedButton } from "./upload-widget-minimized-button";
 
 export function UploadWidget() {
+  const [isWidgetOpen, setIsWidgetOpen] = React.useState(false);
+
   return (
-    <div className="bg-zinc-900 w-full overflow-hidden max-w-[360px] rounded-xl shadow-shape">
-      <UploadWidgetHeader />
+    <Collapsible.Root onOpenChange={setIsWidgetOpen}>
+      <div className="bg-zinc-900 overflow-hidden w-[360px] rounded-xl shadow-shape">
 
-      <div className="flex flex-col gap-4 py-3">
-        <UploadWidgetDropzone />
+        {!isWidgetOpen && <UploadWidgetMinimizedButton />}
+        <Collapsible.Content>
 
-        <div className="h-px bg-zinc-800 border-black/50 box-content"/>
+          <UploadWidgetHeader />
 
-        <UploadWidgetUploadList />
+          <div className="flex flex-col gap-4 py-3">
+            <UploadWidgetDropzone />
+
+            <div className="h-px bg-zinc-800 border-black/50 box-content"/>
+
+            <UploadWidgetUploadList />
+          </div>
+
+        </Collapsible.Content>
       </div>
-    </div>
+    </Collapsible.Root>
   )
-}
+}  
