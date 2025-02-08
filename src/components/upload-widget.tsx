76991fc835cd42c2a8b9a3d58b43cb881e18aee8
Jean Massucatto <massucattoj@Jeans-MacBook-Pro.local>
2025-02-08 10:48:45 -0300

    feat: component structure and biome

--- src/components/upload-widget.tsx
+++ src/components/upload-widget.tsx
@@ -1,32 +1,29 @@
-import * as Collapsible from "@radix-ui/react-collapsible";
-import { UploadWidgetDropzone } from "./upload-widget-dropzone";
-import { UploadWidgetHeader } from "./upload-widget-header";
-import { UploadWidgetUploadList } from "./upload-widget-upload-list";
-import React from "react";
-import { UploadWidgetMinimizedButton } from "./upload-widget-minimized-button";
+import * as Collapsible from '@radix-ui/react-collapsible'
+import React from 'react'
+import { UploadWidgetDropzone } from './upload-widget-dropzone'
+import { UploadWidgetHeader } from './upload-widget-header'
+import { UploadWidgetMinimizedButton } from './upload-widget-minimized-button'
+import { UploadWidgetUploadList } from './upload-widget-upload-list'
 
 export function UploadWidget() {
-  const [isWidgetOpen, setIsWidgetOpen] = React.useState(false);
+  const [isWidgetOpen, setIsWidgetOpen] = React.useState(false)
 
   return (
     <Collapsible.Root onOpenChange={setIsWidgetOpen}>
       <div className="bg-zinc-900 overflow-hidden w-[360px] rounded-xl shadow-shape">
-
         {!isWidgetOpen && <UploadWidgetMinimizedButton />}
         <Collapsible.Content>
-
           <UploadWidgetHeader />
 
           <div className="flex flex-col gap-4 py-3">
             <UploadWidgetDropzone />
 
-            <div className="h-px bg-zinc-800 border-black/50 box-content"/>
+            <div className="h-px bg-zinc-800 border-black/50 box-content" />
 
             <UploadWidgetUploadList />
           </div>
-
         </Collapsible.Content>
       </div>
     </Collapsible.Root>
   )
-}  
+}
