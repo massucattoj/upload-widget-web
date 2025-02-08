d31b29929d24800d57e1ffd998c9001a17ec6a95
Jean Massucatto <massucattoj@Jeans-MacBook-Pro.local>
2025-02-08 12:06:20 -0300

    style: gradient on closed widget

--- src/components/upload-widget-title.tsx
+++ src/components/upload-widget-title.tsx
@@ -1,10 +1,23 @@
 import { UploadCloud } from 'lucide-react'
 
 export function UploadWidgetTitle() {
+  const isThereAnyPendingUpload = true
+  const uploadGlobalPrecentage = 66
+
   return (
     <div className="flex items-center gap-1.5 text-sm font-medium">
       <UploadCloud className="size-4 text-zinc-400" />
-      <span className="text-sm font-medium">Upload files</span>
+
+      {isThereAnyPendingUpload ? (
+        <span className="flex items-baseline gap-1">
+          Uploading{' '}
+          <span className="text-xs text-zinc-400 tabular-nums">
+            {uploadGlobalPrecentage}%
+          </span>
+        </span>
+      ) : (
+        <span>Upload your files</span>
+      )}
     </div>
   )
 }
